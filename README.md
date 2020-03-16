64e02f4aa7cee6d1bc33b8aea8c23076a6a5c098
Andrew <andrew.dale.wylie@gmail.com>
2020-03-16 14:28:22 -0400

    Initial iteration of core game code.

---
+++ README.md
@@ -0,0 +1,43 @@
+## Hangman Game
+
+  |======|        A B C D E F G H I J K L M
+  | /    !
+  |/     O        N O P Q R S T U V W X Y Z
+  |    --|--
+  |      |        H a _ g m a _   G a m _  
+  |     / \
+  |               Invalid guess, try again.
+=====             Enter your guess: b
+
+
+### Requirements
+
+- Game play operates as a regular hangman game.
+
+- Placeholders are displayed for unknown letters of a phrase.
+- The user can make a guess by selecting a letter.
+- If a guess matches an unknown letter, that letter becomes a known letter.
+- Known letters are displayed in the phrase instead of placeholders.
+
+- Each guess that does not match an unknown letter counts as a try.
+- The game is over (a loss) when the user accumulates five (5) tries.
+- The game is over (a win) if all letters of the phrase are known using less that five (5) tries.
+
+- If the user wins they will be congratulated.
+- If the user wins their score will be saved.
+
+- Six phrases are possible: '3dhubs', 'marvin', 'print', 'filament', 'order', 'layer'.
+
+- The game can be played by clients using a simple API.
+- The game can be played by users using an interface.
+
+
+### Derived requirements
+
+- The API provided to clients will be REST-ful.
+- The interface provided to users will be web-based.
+
+- A list of player scores should be displayed in the game's interface.
+
+- Phrases and guesses are assumed to be alpha-numeric only.
+- The case (uppercase/lowercase) of phrases and guesses does not matter.
---
+++ game.py
@@ -0,0 +1,82 @@
+class Hangman:
+    MAXIMUM_TRIES = 5
+
+    def __init__(self, phrase, guesses=None, tries=None):
+        # Allow an existing game to be continued if given all arguments.
+        # TODO: Validate arguments?
+        self.phrase = phrase
+        self.guesses = guesses or {' '}
+        self.tries = tries or 0
+
+    def guess(self, letter):
+        """Allow a user to make a guess, uppercase/lowercase doesn't matter."""
+        self.guesses.add(letter.lower())
+
+        if letter.lower() not in self.phrase.lower():
+            self.tries += 1
+
+    def is_game_over(self):
+        return self.tries >= Hangman.MAXIMUM_TRIES \
+               or set(self.phrase.lower()).issubset(self.guesses)
+
+    def is_game_won(self):
+        return set(self.phrase.lower()).issubset(self.guesses) \
+               and self.tries < Hangman.MAXIMUM_TRIES
+
+    def is_guess_valid(self, guess):
+        # The input guess must be a single character, alphanumeric,
+        # and must not have already been guessed.
+        if len(guess) == 1 \
+                and guess.isalnum() \
+                and guess.lower() not in self.guesses:
+            return True
+
+        return False
+
+    def get_display_phrase(self):
+        """Return the phrase with unknown letters covered."""
+        return ''.join(
+            ['_' if x.lower() not in self.guesses else x for x in self.phrase])
+
+    def get_guesses(self):
+        """Convenience function for GUI (show user which letters are available)."""
+        # Return a copy of the guesses so it can't be modified.
+        return set(self.guesses)
+
+    def get_tries(self):
+        """Convenience function for GUI (drawing the hanged man)."""
+        return self.tries
+
+
+if __name__ == '__main__':
+    import random
+
+
+    def get_input():
+        return input("Enter your guess: ").strip()
+
+
+    phrases = ['3dhubs', 'marvin', 'print', 'filament', 'order', 'layer']
+    game = Hangman(random.choice(phrases))
+
+    while not game.is_game_over():
+
+        print(' '.join(game.get_display_phrase()))
+
+        # Allow the user to make a guess.
+        text = get_input()
+
+        while not game.is_guess_valid(text):
+            print("Invalid guess entered, please try again.")
+            text = get_input()
+
+        game.guess(text)
+
+        if game.is_game_over():
+            if game.is_game_won():
+                print("Congratulations, you win!")
+
+            else:
+                print("Sorry, you lost!")
+
+            exit()
